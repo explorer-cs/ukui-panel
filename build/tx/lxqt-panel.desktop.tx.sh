c21a2ab03d5d8f2b88d5145faa0ec01fbe4e394e
xiaoyiwu <wuxiaoyi@kylinos.cn>
2019-08-24 13:54:17 +0800

    rm build

--- build/tx/lxqt-panel.desktop.tx.sh
+++
@@ -1,7 +0,0 @@
-[ -f /home/kylin/panel/ukui-panel-3.0/autostart/lxqt-panel.desktop.in ] || exit 0
-echo '[lxde-qt.lxqt-panel_desktop]'
-echo 'type = DESKTOP'
-echo 'source_lang = en'
-echo 'source_file = autostart/lxqt-panel.desktop.in'
-echo 'file_filter = autostart/translations/lxqt-panel_<lang>.desktop'
-echo ''
--- build/tx/ukui-panel.desktop.tx.sh
+++
@@ -1,7 +0,0 @@
-[ -f /home/kylin/panel/ukui-panel-3.0/autostart/ukui-panel.desktop.in ] || exit 0
-echo '[lxde-qt.ukui-panel_desktop]'
-echo 'type = DESKTOP'
-echo 'source_lang = en'
-echo 'source_file = autostart/ukui-panel.desktop.in'
-echo 'file_filter = autostart/translations/ukui-panel_<lang>.desktop'
-echo ''
