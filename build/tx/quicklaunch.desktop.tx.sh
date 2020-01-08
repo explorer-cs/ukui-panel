1a8746f6505e677430da26a8c8c776c621829945
hepuyao <2410474020@qq.com>
2020-01-08 21:10:07 +0800

    start

--- build/tx/quicklaunch.desktop.tx.sh
+++
@@ -1,7 +0,0 @@
-[ -f /home/kylin/work/ukui-panel/plugin-quicklaunch/resources/quicklaunch.desktop.in ] || exit 0
-echo '[lxqt.quicklaunch_desktop]'
-echo 'type = DESKTOP'
-echo 'source_lang = en'
-echo 'source_file = plugin-quicklaunch/resources/quicklaunch.desktop.in'
-echo 'file_filter = plugin-quicklaunch/translations/quicklaunch_<lang>.desktop'
-echo ''
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "ukuipanelapplication.h"
-int main (int argc, char *argv[])
-{
-	UkuiPanelApplication app(argc, argv);
-    app.setAttribute(Qt::AA_UseHighDpiPixmaps, true);
-	return app.exec();		
-}
